52c53f7e551d2c6cd4bdfb1e36ef84f1174bbd16
cloby <colbybolby615@gmail.com>
2025-06-06 18:34:21 -0400

    removes the immersive aircraft recipes
    because we don't have this mod anymore

--- kubejs/server_scripts/miscTech.js
+++ kubejs/server_scripts/miscTech.js
@@ -9,27 +9,6 @@ ServerEvents.recipes(event => {
     'waterframes:remote',
     'mekanism:basic_control_circuit'
   )
-  event.remove({output: 'immersive_aircraft:boiler'})
-  event.shaped('immersive_aircraft:boiler', [
-    ' A ', 
-    'ACA', // arg 2: the shape (array of strings)
-    'ABA'  
-  ], {
-    A: '#forge:plates/copper', 
-    B: 'create:blaze_burner',  //arg 3: the mapping object
-    C: 'create:fluid_tank'   
-  })
-  event.replaceInput(
-    { id: 'immersive_aircraft:engine' },
-    'minecraft:blast_furnace',
-    'create:precision_mechanism'
-  )
-  event.remove({output: 'immersive_aircraft:propeller'})
-  event.replaceInput(
-    { input: 'immersive_aircraft:propeller' },
-    'immersive_aircraft:propeller',
-    'create:propeller'
-  )
   event.remove({output: '#thermal:rockwool'})
   event.smelting('#forge:slag', 'immersiveengineering:slag_glass')
   event.shaped(
