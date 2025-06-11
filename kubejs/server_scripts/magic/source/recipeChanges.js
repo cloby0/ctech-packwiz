fa865dc0fb04b3e72b7b7c5f35b5df687de10496
cloby <colbybolby615@gmail.com>
2025-06-10 21:58:58 -0400

    step one of infinite steps of magic recipe tree

--- kubejs/server_scripts/magic/source/recipeChanges.js
+++ kubejs/server_scripts/magic/source/recipeChanges.js
@@ -1,8 +1,55 @@
 ServerEvents.recipes(event => {
     event.recipes.ars_nouveau.imbuement(
-        "witherstormmod:command_block_book", // input item
-        "kubejs:ars_book", // output
-        15000, // source cost
+        "witherstormmod:command_block_book",
+        "kubejs:ars_book",
+        15000,
     []
     )
-  });
+
+    event.recipes.ars_nouveau.imbuement(
+        '#forge:dusts/amethyst',
+        'irons_spellbooks:arcane_essence',
+        500,
+        []
+    )
+
+    event.recipes.create.filling(
+        'irons_spellbooks:blood_vial',
+        [
+        'minecraft:bottle',
+        Fluid.of('bloodmagic:life_essence_fluid_type', 250)
+        ]
+    )
+
+    event.recipes.create.empty(
+        [
+        Fluid.of('bloodmagic:life_essence_fluid_type', 250),
+        'irons_spellbooks:blood_vial'
+        ],
+            'minecraft:bottle'
+        )
+
+    event.replaceInput(
+        { output: 'ars_nouveau:novice_spell_book' },
+        'minecraft:book',
+        'irons_spellbook:iron_spell_book'
+    )
+
+    event.remove({output: 'irons_spellbooks:scroll_forge'})
+     event.shaped(
+        Item.of('irons_spellbooks:scroll_forge', 1), // output item with count
+        [
+        'AAA',
+        'DBE',
+        'CFC'
+        ],
+        {
+        A: 'minecraft:polished_deepslate',
+        B: 'ars_nouveau:scribes_table',
+        C: 'irons_spellbooks:arcane_essence',
+        D: 'minecraft:feather',
+        E: 'irons_spellbooks:common_ink',
+        F: 'minecraft:crying_obsidian'
+        }
+    )
+});
