3b222b9e6e9c47dbfc1cb027b4ff5fccf8e07f52
AutumnBreeze1396 <ajmartin2007@icloud.com>
2025-08-18 22:46:52 -0400

    added 2 T4 recipes

---
+++ kubejs/server_scripts/tech/tier4.js
@@ -0,0 +1,34 @@
+ServerEvents.recipes(event => {
+    //steel casing
+    event.shaped(
+        Item.of("mekanism:steel_casing", 1),
+        [
+            "SGS",
+            "GPG",
+            "SGS"
+        ],
+        {
+            S: "#forge:ingot/steel",
+            G: "#forge:glass",
+            P: "mekanism:pure_osmium_ingot"
+        }
+    )
+    
+    //advanced AE machine rebalances
+    event.shaped(
+        Item.of("advanced_ae:reaction_chamber", 1),
+        [
+            "FEF",
+            "FSF",
+            "GBG"
+        ],
+        {
+            F: "ae2:fluix_dust",
+            E: "ae2:condenser",
+            S: "mekanism:steel_casing",
+            G: "minecraft:glowstone_dust",
+            B: "minecraft:bucket"
+        }
+    )
+
+})
