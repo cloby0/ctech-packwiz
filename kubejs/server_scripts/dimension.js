aa9cc7ffbeb49eb1405bc54d2e11faac680c9e2b
cloby <colbybolby615@gmail.com>
2025-08-31 21:14:47 -0400

    some more mods to ease progression and stuff

---
+++ kubejs/server_scripts/dimension.js
@@ -0,0 +1,214 @@
+ServerEvents.recipes(event => {
+  event.remove({id: "ad_astra:nasa_workbench/tier_1_rocket_from_nasa_workbench"})
+  event.custom({
+  "type": "ad_astra:nasa_workbench",
+  "ingredients": [
+    {
+      "item": "ad_astra:rocket_nose_cone"
+    },
+    {
+      "item": "create:railway_casing"
+    },
+    {
+      "item": "create:railway_casing"
+    },
+    {
+      "item": "create:railway_casing"
+    },
+    {
+      "item": "create:railway_casing"
+    },
+    {
+      "item": "create:railway_casing"
+    },
+    {
+      "item": "create:railway_casing"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:steel_tank"
+    },
+    {
+      "item": "ad_astra:steel_tank"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:steel_engine"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    }
+  ],
+  "result": {
+    "count": 1,
+    "id": "ad_astra:tier_1_rocket"
+  }
+})
+
+  event.remove({id: "ad_astra:nasa_workbench/tier_2_rocket_from_nasa_workbench"})
+  event.custom({
+  "type": "ad_astra:nasa_workbench",
+  "ingredients": [
+    {
+      "item": "ad_astra:rocket_nose_cone"
+    },
+    {
+      "item": "create_tank_defenses:steel_casing"
+    },
+    {
+      "item": "create_tank_defenses:steel_casing"
+    },
+    {
+      "item": "create_tank_defenses:steel_casing"
+    },
+    {
+      "item": "create_tank_defenses:steel_casing"
+    },
+    {
+      "item": "create_tank_defenses:steel_casing"
+    },
+    {
+      "item": "create_tank_defenses:steel_casing"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:steel_tank"
+    },
+    {
+      "item": "ad_astra:steel_tank"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:steel_engine"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    }
+  ],
+  "result": {
+    "count": 1,
+    "id": "ad_astra:tier_2_rocket"
+  }
+})
+
+  event.remove({id: "ad_astra:nasa_workbench/tier_3_rocket_from_nasa_workbench"})
+  event.custom({
+  "type": "ad_astra:nasa_workbench",
+  "ingredients": [
+    {
+      "item": "ad_astra:rocket_nose_cone"
+    },
+    {
+      "item": "thermal:machine_frame"
+    },
+    {
+      "item": "thermal:machine_frame"
+    },
+    {
+      "item": "thermal:machine_frame"
+    },
+    {
+      "item": "thermal:machine_frame"
+    },
+    {
+      "item": "thermal:machine_frame"
+    },
+    {
+      "item": "thermal:machine_frame"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:steel_tank"
+    },
+    {
+      "item": "ad_astra:steel_tank"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:steel_engine"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    }
+  ],
+  "result": {
+    "count": 1,
+    "id": "ad_astra:tier_3_rocket"
+  }
+})
+
+  event.remove({id: "ad_astra:nasa_workbench/tier_4_rocket_from_nasa_workbench"})
+  event.custom({
+  "type": "ad_astra:nasa_workbench",
+  "ingredients": [
+    {
+      "item": "ad_astra:rocket_nose_cone"
+    },
+    {
+      "item": "mekanism:steel_casing"
+    },
+    {
+      "item": "mekanism:steel_casing"
+    },
+    {
+      "item": "mekanism:steel_casing"
+    },
+    {
+      "item": "mekanism:steel_casing"
+    },
+    {
+      "item": "mekanism:steel_casing"
+    },
+    {
+      "item": "mekanism:steel_casing"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:steel_tank"
+    },
+    {
+      "item": "ad_astra:steel_tank"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    },
+    {
+      "item": "ad_astra:steel_engine"
+    },
+    {
+      "item": "ad_astra:rocket_fin"
+    }
+  ],
+  "result": {
+    "count": 1,
+    "id": "ad_astra:tier_4_rocket"
+  }
+})
+});
+
